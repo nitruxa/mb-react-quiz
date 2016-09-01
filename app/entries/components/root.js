518637bf617f54a21b39dde52ebb59e7b69ba0af
Martynas Beinoras <martynas.beinoras@gmail.com>
2016-09-01 23:56:42 +0300

    dev

--- app/entries/components/root.js
+++ app/entries/components/root.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 
 import routes from '../routes';
