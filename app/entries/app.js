b8eff826755e296f928488e51deb775da6eb5b0a
Martynas Beinoras <martynas.beinoras@gmail.com>
2016-09-01 10:39:27 +0300

    dev

--- app/entries/app.js
+++ app/entries/app.js
@@ -5,7 +5,7 @@ import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
 
 import reducer from './reducers';
-import Root from './containers/root.js';
+import Root from './components/root.js';
 
 (function () {
 	
